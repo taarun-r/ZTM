662a5eeab75f3335db1a9eebb42c7d61e0d0e1ed
taarun-r <81550807+taarun-r@users.noreply.github.com>
2025-07-02 21:09:47 +0530

    Chapter-66 completed

---
+++ Python_Basics_2/ternary_operator.py
@@ -0,0 +1,9 @@
+# Ternary Operator - Same as if and elif and else statement, and is a short cut
+# AKA conditional expressions
+#  There are special cases where you can use a ternary operator
+# Syntax: condition_if_true if condition else condition_if_else
+
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+
+print(can_message)
